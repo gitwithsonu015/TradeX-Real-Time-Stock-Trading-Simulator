f71ae097cce719c5c7b22044df3bd478ddd95c44
gitwithsonu015 <sonudigital245@gmail.com>
2026-04-10 20:14:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.NODE_ENV === 'production' 
-      ? 'https://your-vercel-frontend.vercel.app' 
+      ? 'https://trade-x-real-time-stock-trading-sim-five.vercel.app/' 
       : 'http://localhost:5173',
     methods: ['GET', 'POST']
   }
@@ -28,7 +28,7 @@ const io = new Server(server, {
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://your-vercel-frontend.vercel.app' 
+    ? 'https://trade-x-real-time-stock-trading-sim-five.vercel.app/' 
     : 'http://localhost:5173',
   credentials: true
 }));
